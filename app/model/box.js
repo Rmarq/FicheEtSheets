36ed702139427b80afc9980faf8349ac1bf3cb0b
Rémi MARQUERIE <remi.marquerie@gmail.com>
2025-01-29 17:00:00 +0100

    Added the 'kind' attribute to Box model.

--- app/model/box.js
+++ app/model/box.js
@@ -7,22 +7,29 @@ class Box extends Model { }
 
 Box.init(
     {
-        id: {
+        id: { // traditionnal unique id
             type: DataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true,
             allowNull: false,
         },
-        title: {
+        title: { // The title of the box shown in the hmi
             type: DataTypes.STRING,
         },
-        details: {
+        kind: { // There will be different kind of boxes for different behavior
+            type: DataTypes.STRING,
+			defaultValue: "Basic", // Basic boxes to contain other boxes or cards
+			// Other values :
+			//  - Tasks : Box containing dated tasks which can be recuring
+			//  - Learning : Box containing flash cards that can be answered. If correctly answered, the card will be shown later, else it is shown the day after. Leitner.
+        },
+        details: { // 
             type: DataTypes.STRING,
         },
-        parentId: {
+        parentId: { // Id of the parent box. Null if it is a top box.
             type: DataTypes.INTEGER,
         },
-        childrenIds: {
+        childrenIds: { // Ids of the boxes contained inside this one.
             type: DataTypes.ARRAY(DataTypes.INTEGER),
         },
     },
